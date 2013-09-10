88ecb0f7cfdeb4ded7573682aa192989227803c3
UUUEEELLL <uuueeelll@gmail.com>
2013-09-10 17:40:21 +0400

    Main template is done

--- myJs.js
+++ myJs.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
    
    
     
-    $('#productsMenuWrp').affix({
+    $('#productsMenu').affix({
         offset: {
           top: 150
         , bottom: function () {
