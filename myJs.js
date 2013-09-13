d195ee49ee74cd50642382adc09fd7c269af9eca
UUUEEELLL <uuueeelll@gmail.com>
2013-09-13 17:20:38 +0400

    Main template Published

--- myJs.js
+++ myJs.js
@@ -5,8 +5,9 @@ $(document).ready(function() {
     $('#productsMenu').affix({
         offset: {
           top: 150
+          
         , bottom: function () {
-            return (this.bottom = $('.bs-footer').outerHeight(true))
+            return (this.bottom = $('#bs-footer').outerHeight(true))
           }
         }
     });
